20e8e3c1cc07b5ad8bcba6f17988e4ab50dc1f59
Blair Tingey <blairt@xmission.com>
2018-02-25 04:07:40 -0700

    Added additional functionality when viewing product details.

--- src/app/products/product-detail.component.ts
+++ src/app/products/product-detail.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {IProduct} from './products';
+import {ActivatedRoute, Router} from '@angular/router';
+import { ProductService } from './product.service';
 
 @Component({
   templateUrl: './product-detail.component.html',
@@ -8,9 +10,26 @@ import {IProduct} from './products';
 export class ProductDetailComponent implements OnInit {
   pageTitle: string = 'Product Detail';
   product: IProduct;
-  constructor() { }
+  errorMessage: String;
+  constructor(private _route: ActivatedRoute,
+              private _router: Router,
+              private _productService: ProductService) { }
 
   ngOnInit() {
+    const param = this._route.snapshot.paramMap.get('id');
+    if (param) {
+      const id = +param;
+      this.getProduct(id);
+    }
+
+  }
+  getProduct(id: number) {
+    this._productService.getProduct(id).subscribe(
+      product => this.product = product,
+      error => this.errorMessage = <any>error);
   }
 
+  onBack(): void {
+    this._router.navigate(['/products']);
+  }
 }
